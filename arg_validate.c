8a0930448cebfe600172363c8494b5d1a0d4400d
Kavini Weerasinghe <131370963+kavini9@users.noreply.github.com>
2024-11-10 14:35:14 +0200

    Create arg_validate.c

---
+++ arg_validate.c
@@ -0,0 +1,54 @@
+#include "fractol.h"
+
+int	is_float(char *str)
+{
+	int	dec_flag;
+	int	digit_count;
+
+	dec_flag = 0;
+	digit_count = 0;
+	if (*str == '+' || *str == '-')
+		str++;
+	while (*str)
+	{
+		if (!ft_isdigit((unsigned char) *str) && *str != '.')
+			return (0);
+		else if (*str == '.')
+		{
+			if (dec_flag || !digit_count)
+				return (0);
+			dec_flag = digit_count;
+		}
+		else
+			digit_count++;
+		str++;
+	}
+	return (digit_count - dec_flag);
+}
+
+int	is_valid(int ac, char **av)
+{
+	if (ac < 2 || ac == 3 || ac > 4)
+		return (arg_guide("Invalid number of arguments"));
+	if (!ft_strcmp(av[1], "mandelbrot"))
+	{
+		if (ac != 2)
+			return (arg_guide("Invalid number of arguments"));
+		else
+			return (1);
+	}
+	else if (!ft_strcmp(av[1], "julia"))
+	{
+		if (ac != 4)
+			return (arg_guide("Invalid number of arguments"));
+		else
+		{
+			if (!is_float(av[2]) || !is_float(av[3]))
+				return (arg_guide("Invalid argument type."));
+			else
+				return (1);
+		}
+	}
+	else
+		return (arg_guide("Invalid argument: <fractal_set>"));
+}
